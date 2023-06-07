4d302a1f03d8241bb012de87846a151df49d8032
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2023-06-07 23:20:55 +0530

    Digit Frequency

--- Digit_Frequency.java
+++ Digit_Frequency.java
@@ -22,15 +22,4 @@ public class Digit_Frequency
 	    
 	    return finalCount;
 	}
-	
-	
-	public static int countDigit(int n){
-	    int count = 0;
-	    while(n > 0){
-	        n = n/10;
-	        count++;
-	    }
-	    
-	    return count;
-	}
 }
