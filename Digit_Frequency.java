a72a33e892db457224cf47b84ab1768d628ed979
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2023-06-07 23:19:58 +0530

    Digit Frequency

---
+++ Digit_Frequency.java
@@ -0,0 +1,36 @@
+public class Digit_Frequency
+{
+	public static void main(String[] args) {
+		int num = 98439862;
+		int search = 9;
+		
+		System.out.println(countDigit(num));
+		System.out.println(searchNumber(num, search));
+	}
+	
+	public static int searchNumber(int num, int search){
+	    int count = countDigit(num) - 1;
+	    int finalCount = 0;
+	    while(num != 0){
+	        int rem = num % 10;
+	        if(rem == search){
+	            finalCount++;
+	        }
+	        num = num / 10;
+	        count--;
+	    }
+	    
+	    return finalCount;
+	}
+	
+	
+	public static int countDigit(int n){
+	    int count = 0;
+	    while(n > 0){
+	        n = n/10;
+	        count++;
+	    }
+	    
+	    return count;
+	}
+}
